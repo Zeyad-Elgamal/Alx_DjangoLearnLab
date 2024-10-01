ec4112d809402579ef0955ea33ab7241dacb13d4
Zeyad-Elgamal <zz67ee@gmail.com>
2024-10-01 07:02:59 +0300

    Implement Author, Book, Library, and Librarian models

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,28 +1,29 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
