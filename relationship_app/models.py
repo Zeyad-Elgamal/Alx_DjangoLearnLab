d3517ff1a52ea97f65ebf527f572876005c2d43e
Zeyad-Elgamal <zz67ee@gmail.com>
2024-10-01 06:55:10 +0300

    Define Author, Book, Library, and Librarian models

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -11,7 +7,7 @@ class Author(models.Model):
         return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -30,30 +26,3 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
-    
-
-
-class UserProfile(models.Model):
-    ROLE_CHOICES = (
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    )
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-
-    def __str__(self):
-        return f'{self.user.username} - {self.role}'
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
-
-"class Meta", "permissions"
-"can_add_book", "can_change_book", "can_delete_book"
