48edcfb65a90eddae42b91bf6b62b92945e6c349
Zeyad-Elgamal <zz67ee@gmail.com>
2024-10-03 09:13:21 +0300

    Implemented role-based access control with user profiles and restricted views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -33,3 +33,33 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile # type: ignore
+
+# Check if the user is an admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+# Check if the user is a librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+# Check if the user is a member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
