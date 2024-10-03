533a99e20dbc6fac367d120dc4d59bec36b2de48
Zeyad-Elgamal <zz67ee@gmail.com>
2024-10-03 03:10:47 +0300

    Updated README to reflect new structure

--- django-models/django-models/relationship_app/urls.py
+++
@@ -1,8 +0,0 @@
-# relationship_app/urls.py
-
-from django.urls import path
-from . import views  # Ensure this import is correct
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Ensure this points to an existing view
-]
--- django-models/django-models/relationship_app/views.py
+++
@@ -1,6 +0,0 @@
-# relationship_app/views.py
-
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'template_name.html')  # Change 'template_name.html' to your actual template
