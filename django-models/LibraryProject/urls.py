8644074b88097f7cd61f95b2ef0d15bfc43a31fc
Zeyad-Elgamal <zz67ee@gmail.com>
2024-10-03 03:06:21 +0300

    Updated README to reflect new structure

--- django-models/LibraryProject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from relationship_app.views import index  # Import the index view
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),  # Redirect the root URL to the index view
-    path('relationship_app/', include('relationship_app.urls')),  # Include your app's URL patterns
-]
