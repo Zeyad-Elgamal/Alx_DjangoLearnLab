87a9823edec4b93d10d5304a24aee87b8e79bcbd
Zeyad-Elgamal <zz67ee@gmail.com>
2024-10-06 06:45:15 +0300

    Fix: Added CustomUser model in bookshelf/models.py

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -1,2 +1,19 @@
 AUTH_USER_MODEL = 'UserProfile.CustomUser'  # adjust according to your app name
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bookshelf',  # Add your app here
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', # type: ignore
+    }
+}
