fc53b930457893a00799d2677a34b29282cbf347
Zeyad-Elgamal <zz67ee@gmail.com>
2024-10-06 06:36:19 +0300

    Fix: Added CustomUser model in bookshelf/models.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    class Meta:
+        permissions = [
+            ('can_view_profile', 'Can view profile'),
+        ]
--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -1,1 +1,2 @@
 AUTH_USER_MODEL = 'UserProfile.CustomUser'  # adjust according to your app name
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
