c1adc22a50db6fddb78524c98f8169f49b94e89e
Zeyad-Elgamal <zz67ee@gmail.com>
2024-10-06 07:35:13 +0300

    Added book_list view and template

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -11,3 +11,21 @@ def create_book(request):
 def delete_book(request, book_id):
     # Logic to delete a book
     return render(request, 'bookshelf/delete_book.html')
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # Logic to create a book
+    return render(request, 'bookshelf/create_book.html')
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    # Logic to delete a book
+    return render(request, 'bookshelf/delete_book.html')
+
+@permission_required('bookshelf.view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # Get all books from the database
+    return render(request, 'bookshelf/book_list.html', {'books': books})
